d46ba2ab57d0346878bda77d5a97563fee4b15d4
vadym.polianskyi <Vadym.Polianskyi@playtech.com>
2021-06-09 12:00:46 -0400

    Correct SwapNodesInPairs

--- src/test/scala/SwapNodesInPairsTest.scala
+++ src/test/scala/SwapNodesInPairsTest.scala
@@ -4,22 +4,22 @@ import SwapNodesInPairs.ListNode
 
 import org.scalatest.funsuite.AnyFunSuite
 
-class SwapNodesInPairsTest  extends AnyFunSuite  {
+class SwapNodesInPairsTest extends AnyFunSuite {
 
   private val swapper = new SwapNodesInPairs()
 
   test("Swap in single list") {
-    val res = swapper.swapPairs(new ListNode(1))
-    assertResult(new ListNode(1))(res)
+    val res = swapper.swapPairs(ListNode(1))
+    assertResult(ListNode(1))(res)
   }
 
   test("Swap in list 1") {
-    val res = swapper.swapPairs(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4)))))
-    assertResult(new ListNode(2, new ListNode(1, new ListNode(4, new ListNode(3)))))(res)
+    val res = swapper.swapPairs(ListNode(1, ListNode(2, ListNode(3, ListNode(4)))))
+    assertResult(ListNode(2, ListNode(1, ListNode(4, ListNode(3)))))(res)
   }
 
   test("Remove from empty list") {
-    val res = swapper.swapPairs(new ListNode())
-    assertResult(new ListNode())(res)
+    val res = swapper.swapPairs(ListNode())
+    assertResult(ListNode())(res)
   }
 }
