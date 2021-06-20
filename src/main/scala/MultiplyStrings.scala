3b6e5b0d0bbd01dfb9653f97b2d01a3d10b3edc8
vadym.polianskyi <Vadym.Polianskyi@playtech.com>
2021-06-20 08:33:50 -0400

    Finished MultiplyStrings with tests

--- src/main/scala/MultiplyStrings.scala
+++ src/main/scala/MultiplyStrings.scala
@@ -2,22 +2,56 @@ package com.polianskyi.leetcode
 
 class MultiplyStrings {
   def multiply(num1: String, num2: String): String = {
-    seqMultiply(num1.reverse, num2.reverse).toString
+    val res = seqMultiply(num1.reverse, num2.reverse).reverse
+    if (res.startsWith("0")) {
+      val n = res.dropWhile(_ == '0')
+      if (n.nonEmpty) n
+      else "0"
+    } else res
   }
 
-  def seqMultiply(num1: String, num2: String, i1: Int = 0, i2: Int = 0): Int = {
+  def seqMultiply(num1: String, num2: String, i2: Int = 0): String = {
     if (i2 < num2.length) {
-      if (i1 < num1.length) {
-        val mlt = correctedWithPosition(i1, num1(i1).toString.toInt) * correctedWithPosition(i2, num2(i2).toString.toInt)
-        mlt + seqMultiply(num1, num2, i1 + 1, i2)
-      } else {
-        seqMultiply(num1, num2, 0, i2 + 1)
-      }
-    } else 0
+      val res = List.fill(i2)("0").mkString("") + multiplyForSingle(num1, num2(i2).toString.toInt)
+      val resN = seqMultiply(num1, num2, i2 + 1)
+      val sum = add(res, resN)
+      sum
+    } else ""
   }
 
-  def correctedWithPosition(i: Int, n: Int): Int = {
-    if (i == 0) n
-    else 10 * correctedWithPosition(i - 1, n)
+  def multiplyForSingle(num1: String, multiplier: Int, rest: Int = 0, i1: Int = 0): String = {
+    if (i1 < num1.length) {
+      val mlt = num1(i1).toString.toInt * multiplier + rest
+      val (newRest, newMlt) = separateRest(mlt)
+      s"$newMlt${multiplyForSingle(num1, multiplier, newRest, i1 + 1)}"
+    } else {
+      if (rest != 0) rest.toString
+      else ""
+    }
   }
+
+  def add(num1: String, num2: String, i: Int = 0, rest: Int = 0): String = {
+    if (i < num1.length && i < num2.length) {
+      val n1 = num1(i).toString.toInt
+      val n2 = num2(i).toString.toInt
+      val (newRest, sum) = separateRest(n1 + n2 + rest)
+      s"$sum${add(num1, num2, i + 1, newRest)}"
+    } else if (i < num1.length) {
+      val (newRest, sum) = separateRest(num1(i).toString.toInt + rest)
+      s"$sum${add(num1, num2, i + 1, newRest)}"
+    } else if (i < num2.length) {
+      val (newRest, sum) = separateRest(num2(i).toString.toInt + rest)
+      s"$sum${add(num1, num2, i + 1, newRest)}"
+    } else {
+      if (rest != 0) rest.toString
+      else ""
+    }
+  }
+
+  def separateRest(sum: Int): (Int, Int) =
+    if (sum >= 10) {
+      val rest = sum / 10
+      val newSum = sum - (rest * 10)
+      (rest, newSum)
+    } else (0, sum)
 }
--- src/test/scala/MultiplyStringsTest.scala
+++ src/test/scala/MultiplyStringsTest.scala
@@ -16,6 +16,26 @@ class MultiplyStringsTest extends AnyFunSuite {
     assertResult("56088")(res)
   }
 
+  test("Multiply sums 3") {
+    val res = multiplier.multiply("235", "103")
+    assertResult("24205")(res)
+  }
+
+  test("Multiply sums 4") {
+    val res = multiplier.multiply("9133", "0")
+    assertResult("0")(res)
+  }
+
+  test("Multiply sums 5") {
+    val res = multiplier.multiply("140", "721")
+    assertResult("100940")(res)
+  }
+
+  test("Multiply sums 6") {
+    val res = multiplier.multiply("123456789", "987654321")
+    assertResult("121932631112635269")(res)
+  }
+
 
 }
 
