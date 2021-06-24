f9ed54eaa25855d494e5457aec32cb510acabc00
vadym.polianskyi <Vadym.Polianskyi@playtech.com>
2021-06-24 08:12:53 -0400

    Improved permuntations algorithm

--- src/main/scala/Permutations.scala
+++ src/main/scala/Permutations.scala
@@ -3,16 +3,8 @@ package com.polianskyi.leetcode
 class Permutations {
   def permute(nums: Array[Int]): List[List[Int]] = perm(nums).distinct
 
-  def perm(nums: Array[Int], i1: Int = 0): List[List[Int]] =
-    if (i1 > nums.length - 1) List(nums.toList)
-    else nums.indices.flatMap(i => perm(swap(nums, i1, i), i1 + 1)).toList
-
-
-  def swap(nums: Array[Int], n: Int, m: Int): Array[Int] = {
-    val copy: Array[Int] = nums.map(f => f)
-    val nEl = copy(n)
-    copy.update(n, copy(m))
-    copy.update(m, nEl)
-    copy
-  }
+  def perm(nums: Array[Int], i1: Int = 0, used: Set[Int] = Set(), local: List[Int] = List()): List[List[Int]] =
+    if (i1 > nums.length - 1) List(local)
+    else nums.indices.filter(!used.contains(_))
+        .flatMap(v => perm(nums, i1 + 1, used + v, nums(v) :: local)).toList
 }
